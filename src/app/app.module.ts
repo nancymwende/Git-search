72d05bc4ffd2774cced48118d2f364843c78e566
bkee9 <nancy.mwende@student.moringaschool.com>
2022-04-17 16:37:56 +0300

    imported the APIService from services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APIService } from './services/api.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GithubComponent } from './components/github/github.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NavbarComponent, GithubComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [APIService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
