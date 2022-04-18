5496c3fa05984729bdfbe3d9429e99ea6c1c53c9
bkee9 <nancy.mwende@student.moringaschool.com>
2022-04-19 00:46:55 +0300

    modifies the component.ts

--- src/app/components/github/github.component.ts
+++ src/app/components/github/github.component.ts
@@ -4,19 +4,40 @@ import { APIService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-github',
   templateUrl: './github.component.html',
-  styleUrls: ['./github.component.css']
+  styleUrls: ['./github.component.css'],
 })
 export class GithubComponent implements OnInit {
-  public User:any=[];
+  public User: any = [];
+  public repos: any = [];
+  public Username ="";
 
-  constructor(private APIService:APIService) {
-    this.APIService.getUserdetails().subscribe((response:any)=>{
-      this.User=response;
-      console.log (response);
-    })
-   }
-
-  ngOnInit(): void {
+  constructor(private APIService: APIService) {
+    this.APIService.getUserdetails().subscribe((response: any) => {
+      this.User = response;
+    });
+    this.APIService.getRepos().subscribe((response: any) => {
+      this.repos = response;
+      console.log(response);
+    });
   }
+  
 
+    findUser(){
+      this.APIService.getUsername(this.Username)
+      this.APIService.getUserdetails().subscribe((response: any) => {
+      this.User = response;
+    })
+  
+    this.APIService.getRepos().subscribe((response: any) => {
+      this.repos = response;
+      console.log(response);
+    })
+
+}
+
+
+
+ngOnInit(): void {
+  
+  }
 }
