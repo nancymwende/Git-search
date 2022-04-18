47fc5fc8b9fb35151f1edb2e13db481ebcd8daed
bkee9 <nancy.mwende@student.moringaschool.com>
2022-04-18 22:49:40 +0300

    initialised the repositories function

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,31 +4,42 @@ import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class APIService {
-  private username:string;
-  private user:any;
-  
+  private username: string;
+  private user: any;
 
-  constructor(private http:HttpClient) {
-    console.log ("service is now ready")
-    this.username = "nancymwende";
-
-    
-   }
-   getUserdetails(){
-     return this.http.get('https://api.github.com/users/' + this.username).pipe(map(data=>{
-       return data;
-     }));
-    
-   }
+  constructor(private http: HttpClient) {
+    console.log('service is now ready');
+    this.username = 'nancymwende';
+  }
+  getUserdetails() {
+    return this.http.get('https://api.github.com/users/' + this.username).pipe(
+      map((data) => {
+        return data;
+      })
+    );
+  }
   getRepos() {
-    return this.http.get("https://api.github.com/users/" + this.username + "/repos?api_key=" + environment.api).pipe(map(data => {
-      return data;
-    }));  
+    return this.http
+      .get(
+        'https://api.github.com/users/' +
+          this.username +
+          '/repos?api_key=' +
+          environment.api
+      )
+      .pipe(
+        map((data) => {
+          return data;
+        })
+      );
+  }
+  getUsername(username: string) {
+    this.username = username;
+  }
 }
-}
+
      
      
      
