d9f972fe76501b7744dcdc20435b2b1a66c2e26b
bkee9 <nancy.mwende@student.moringaschool.com>
2022-04-19 00:20:01 +0300

    adds the formsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APIService } from './services/api.service';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GithubComponent } from './components/github/github.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReposComponent } from './components/repos/repos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, GithubComponent, ReposComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [APIService],
   bootstrap: [AppComponent],
 })
