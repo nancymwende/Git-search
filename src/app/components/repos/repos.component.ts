f72ebe64bc03bfb5fdee289f53d64c1cdf6a5c0e
bkee9 <nancy.mwende@student.moringaschool.com>
2022-04-19 00:22:48 +0300

    assigns the constructor a value

--- src/app/components/repos/repos.component.ts
+++ src/app/components/repos/repos.component.ts
@@ -7,14 +7,10 @@ import { APIService } from 'src/app/services/api.service';
   styleUrls: ['./repos.component.css']
 })
 export class ReposComponent implements OnInit {
-  public repos:any = []
+ 
 
   constructor(private APIService:APIService) {
-    this.APIService.getRepos().subscribe((response:any)=>{
-      this.repos=response;
-      console.log (response);
-    })
-
+    
    }
 
   ngOnInit(): void {
